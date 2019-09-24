6d1cf9d1ca44a6b979449463fe14741e5ec2ca4d
arlo-luong-goldenowl <arlo-luong-goldenowl@gmail.com>
2019-09-24 11:15:50 +0700

    user login using devise, before action for dashboard controller

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Rails6Project
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
