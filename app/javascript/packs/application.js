bc73646e9186611a64bd4c37bff19b8201350fc1
arlo-luong-goldenowl <arlo-luong-goldenowl@gmail.com>
2019-09-26 17:23:17 +0700

    restucture assets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import "assets/css/vendor/jquery-jvectormap-1.2.2.css"
-import "assets/css/icons.css"
-import "assets/css/app.css"
+import "hyper/css/vendor/jquery-jvectormap-1.2.2.css"
+import "hyper/css/icons.css"
+import "hyper/css/app.css"
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -17,11 +17,11 @@ require("select2")
 require("jquery-slimscroll")
 
 $(document).on('ready turbolinks:load', function() {
-  require("assets/js/app.min")
-  require("assets/js/vendor/Chart.bundle.min")
-  require("assets/js/vendor/jquery-jvectormap-1.2.2.min")
-  require("assets/js/vendor/jquery-jvectormap-world-mill-en")
-  require("assets/js/pages/demo.dashboard")
+  require("hyper/js/app.min")
+  require("hyper/js/vendor/Chart.bundle.min")
+  require("hyper/js/vendor/jquery-jvectormap-1.2.2.min")
+  require("hyper/js/vendor/jquery-jvectormap-world-mill-en")
+  require("hyper/js/pages/demo.dashboard")
 })
 
 
@@ -29,5 +29,12 @@ $(document).on('ready turbolinks:load', function() {
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+
+// console.log(require.context)
+// const requireAll = (r) => r.keys().map(r)
+// requireAll(require.context('hyper/images', true, /\.(png|jpe?g|svg)$/));
+
+require.context('../images', true);
+require.context('hyper/images', true);
+// const imagePath = (name) => hyperImages(name, true)
 // const imagePath = (name) => images(name, true)
